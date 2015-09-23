184496cd8f9e64a9721b178cebf459a951f3a67d
Abhishek Gupta <abhi.bansal21@gmail.com>
2015-09-23 15:55:57 +0530

    Add a Contact page (solves exercise 3.2)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
   test 'should get home' do
@@ -21,5 +21,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select 'title', "About | #{@base_title}"
+  end
+
+  test 'should get contact' do
+    get :contact
+    assert_response :success
+    assert_select 'title', "Contact | #{@base_title}"
   end
 end
